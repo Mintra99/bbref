9272e04125e8a5a7a7ca9e2a5aafd0a01e5b22b0
Minh Christian Tran <54357939+Mintra99@users.noreply.github.com>
2022-09-01 09:20:47 +0200

    created stats table

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,30 +3,31 @@ import Banner from "react-banner";
 import "react-banner/dist/style.css";
 
 function Nav() {
-        return (
-            <Banner
-            logo = "My Logo"
-            url={window.location.pathname}
-            items={[
-                {content: "Stats", url: "/stats"},
-                {content: "About", url: "/about"},
-                {content: "Shop", url: "/shop"},
-                {content: "Link with children", 
-                url: "/children", 
-                children: [
-                    {content: "child1", url:"/children/child1"},
-                    {content: "child2", url:"/children/child2"},
-                    {content: "child3", url:"/children/child3"}
-                ]}
-            ]}
-            />
-        );
+  return (
+    <Banner
+      logo="My Logo"
+      url={window.location.pathname}
+      items={[
+        { content: "Stats", url: "/stats" },
+        { content: "About", url: "/about" },
+        { content: "Shop", url: "/shop" },
+        {
+          content: "Link with children",
+          url: "/children",
+          children: [
+            { content: "child1", url: "/children/child1" },
+            { content: "child2", url: "/children/child2" },
+            { content: "child3", url: "/children/child3" },
+          ],
+        },
+      ]}
+    />
+  );
 }
 
 export default Nav;
 
-
-// ------------------------ Old code ------------------------ 
+// ------------------------ Old code ------------------------
 // import { Link } from "react-router-dom";
 // function Nav() {
 //   return (
--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -1,33 +1,95 @@
-import React from "react";
-import { CsvToHtmlTable } from "react-csv-to-table";
+import React, { useState, useEffect, useRef } from "react";
+import East_conf_2022 from "../data/df_2022_East.csv";
+import West_conf_2022 from "../data/df_2022_West.csv";
+import Papa from "papaparse";
+import "bootstrap/dist/css/bootstrap.min.css";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
+import Table from "react-bootstrap/Table";
 
-const data = [
-    { name: "Anom", age: 19, gender: "Male" },
-    { name: "Megha", age: 19, gender: "Female" },
-    { name: "Subham", age: 25, gender: "Male"},
-  ]
+export default function Stats() {
+  const [parsedCsvData, setParsedCsvData] = useState([]);
+  const [sortedField, setSortedField] = useState(null);
+  const [value, setValue] = useState("East");
+  const csv_data = useRef(East_conf_2022);
 
-function Stats() {
-    return (
-        <div className="Stats-table">
-          <table>
-            <tr>
-              <th>Name</th>
-              <th>Age</th>
-              <th>Gender</th>
-            </tr>
-            {data.map((val, key) => {
-              return (
-                <tr key={key}>
-                  <td>{val.name}</td>
-                  <td>{val.age}</td>
-                  <td>{val.gender}</td>
-                </tr>
-              )
-            })}
-          </table>
-        </div>
-      );
+  useEffect(() => {
+    // Selects conference
+    if (value === "East") {
+      csv_data.current = East_conf_2022;
+    } else {
+      csv_data.current = West_conf_2022;
+    }
+
+    // Loads csv file
+    async function getData() {
+      const response = await fetch(csv_data.current);
+      const reader = response.body.getReader();
+      const result = await reader.read(); // raw array
+      const decoder = new TextDecoder("utf-8");
+      const csv = decoder.decode(result.value); // the csv text
+      const results = Papa.parse(csv, { header: true }); // object with { data, errors, meta }
+      const rows = results.data; // array of objects
+      setParsedCsvData(rows);
+    }
+    getData();
+  }, [value]);
+
+  return (
+    <div className="Stats" style={{ margin: "20px" }}>
+      <DropdownButton
+        variant="dark"
+        title="Select a conference"
+        id="dropdown-menu-align-right"
+        onSelect={(e) => setValue(e)}
+      >
+        <Dropdown.Item eventKey={"East"}>East</Dropdown.Item>
+        <Dropdown.Item eventKey={"West"}>West</Dropdown.Item>
+      </DropdownButton>
+      <h2>{value}ern conference</h2>
+      <Table className="Statstable" striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>
+              <button type="button" onClick={() => setSortedField("Team")}>
+                Teams
+              </button>
+            </th>
+            <th>
+              <button type="button" onClick={() => setSortedField("W")}>
+                Wins
+              </button>
+            </th>
+            <th>
+              <button type="button" onClick={() => setSortedField("L")}>
+                Losses
+              </button>
+            </th>
+            <th>
+              <button type="button" onClick={() => setSortedField("GB")}>
+                GB
+              </button>
+            </th>
+            <th>
+              <button type="button" onClick={() => setSortedField("Playoffs")}>
+                Playoffs
+              </button>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {parsedCsvData &&
+            parsedCsvData.map((parsedData, index) => (
+              <tr key={index}>
+                <td>{parsedData.Team}</td>
+                <td>{parsedData.W}</td>
+                <td>{parsedData.L}</td>
+                <td>{parsedData.GB}</td>
+                <td>{parsedData.Playoffs}</td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
+    </div>
+  );
 }
-
-export default Stats;
